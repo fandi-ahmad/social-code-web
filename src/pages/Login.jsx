129506ac272f56bd10083b134c9013f5d65552e5
fandi-ahmad <fandi4160@gmail.com>
2023-06-12 16:01:38 +0800

    add login user

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const navigate = useNavigate()
+
+  const loginUser = async () => {
+    try {
+      const respons = await axios.post('http://localhost:8000/api/v1/login', {
+        username: username,
+        password: password
+      })
+      console.log(respons)
+      navigate('/')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <>
       <div>
         <h1>Login</h1>
-        <input type="text" placeholder='username' />
+        <input type="text" placeholder='username'
+          value={username} onChange={(e) => setUsername(e.target.value)}
+        />
         <br /> <br />
-        <input type="password" placeholder='password' />
+        <input type="password" placeholder='password'
+          value={password} onChange={(e) => setPassword(e.target.value)}
+        />
         <br /> <br />
-        <button>login</button>
+        <button onClick={loginUser}>login</button>
       </div>
     </>
   );
